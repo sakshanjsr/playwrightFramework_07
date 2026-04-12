a5e409fa6cb723073fb2074caa8d5cdf0c5ea3df
sakshanjsr <sakshanjsr@gmail.com>
2026-04-12 15:23:33 +0530

    smoke changes in yml

--- src/tests/loginParent.spec.js
+++ src/tests/loginParent.spec.js
@@ -3,13 +3,10 @@ const logindata = require("../../src/testdata/logindata.json")
 
 
 
-test("Login to Application" , async function({loginPage}){
+test("Login to Application @smoke @regression" , async function({loginPage}){
 
      const authFile = "src/config/auth.json"
 
-     //await page.goto("https://practicetestautomation.com/practice-test-login/")
-     //Creating obj of the loginPage class
-     //const loginpage = new LoginPage(page)
 
      console.log("Environment Selected : " + process.env.ENV);
      console.log("User ID from env file is : " + process.env.userid);
---
+++ src/tests/practice.spec.js
@@ -0,0 +1,22 @@
+
+
+const {test, expect} = require('@playwright/test')
+
+test.describe("Practice Tests", () => {
+
+    test("Get CSS Value @smoke", async ({page}) => {
+        await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
+        const button = page.getByRole('Button' , {name : 'START'} )
+        const color = await button.evaluate(e1 => getComputedStyle(e1).backgroundColor)
+        console.log("Button Color : "+color)
+    })
+
+    test("Handle Alerts @smoke", async ({page}) => {
+        await page.goto("https://the-internet.herokuapp.com/javascript_alerts")
+        page.on('dialog' , async (alertWindow) => {
+            expect(alertWindow.type()).toContain("alert")
+        })
+        await page.locator("//button[contains(text(),'Alert')]").click()
+    })
+
+})
